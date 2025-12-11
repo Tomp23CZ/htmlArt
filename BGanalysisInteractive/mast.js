2798580e90766ea7109ad60be4af9f9bf263bd77
nosek.tomas <nosek.tomas@ssakhk.cz>
2025-12-11 08:04:53 +0100

    finishing touch

--- BGanalysisInteractive/mast.js
+++
@@ -1,7 +0,0 @@
-const masts = document.querySelectorAll("mast")
-
-masts.forEach(mast => {
-    const clone = mast.cloneNode(false)
-    clone.setAttribute("class", "mast-outline")
-    mast.appendChild(clone)
-});
